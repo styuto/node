964a4307e7a1bd6ade50defe1b3373a37e43a4df
Jesus Arturo Ceballos Rendon <arturocr1992@gmail.com>
2021-08-06 01:18:22 -0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>res.send('Hello World'));
+app.get('/', (req, res) =>res.sendFile("/index.html"));
 
 app.listen(3000);
 console.log('Server on port', 3000);
